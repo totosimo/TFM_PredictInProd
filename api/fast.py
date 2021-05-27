2b2224f85380e75a0069136a812c1e56ef8cefc9
Simone Totino <totino.simone@gmail.com>
2021-05-27 16:31:51 +0300

    predict based on locally trained model

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from datetime import datetime
+import pytz
+import joblib
 
 app = FastAPI()
 
@@ -14,3 +18,45 @@ app.add_middleware(
 @app.get("/")
 def index():
     return {"greeting": "Hello world"}
+
+@app.get("/predict")
+def predict(pickup_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count):
+
+    # create datetime object from user provided date
+    pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+    
+    # localize the user provided datetime with the NYC timezone
+    eastern = pytz.timezone("US/Eastern")
+    localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+    
+    # convert the user datetime to UTC
+    utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+    
+    # format the datetime as expected by the pipeline
+    formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
+    
+    # fixing a value for the key, unused by the model
+    # in the future the key might be removed from the pipeline input
+    # eventhough it is used as a parameter for the Kaggle submission
+    key = "2013-07-06 17:18:00.000000119"
+    
+    # build X ⚠️ beware to the order of the parameters ⚠️
+    X = pd.DataFrame(dict(
+        key=[key],
+        pickup_datetime=[formatted_pickup_datetime],
+        pickup_longitude=[float(pickup_longitude)],
+        pickup_latitude=[float(pickup_latitude)],
+        dropoff_longitude=[float(dropoff_longitude)],
+        dropoff_latitude=[float(dropoff_latitude)],
+        passenger_count=[int(passenger_count)]))
+    
+    # pipeline = get_model_from_gcp()
+    pipeline = joblib.load('model.joblib')
+    
+    # make prediction
+    results = pipeline.predict(X)
+    
+    # convert response from numpy to python type
+    pred = float(results[0])
+    
+    return dict(prediction=pred)
